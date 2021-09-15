f3ec5b927ad54a99caa82c030d8e43d3e2762b4f
Thiago Destazio <88913979+Thiagozera@users.noreply.github.com>
2021-09-15 12:51:39 -0300

    Add files via upload

---
+++ Projeto-Urna-Eletronica/Códigos/etapas.js
@@ -0,0 +1,11 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos : [
+     {
+       numero: 33111
+       name: 'ALEATÓRIO DA SILVA'
+       partido: 'EBF'
+       fotos: [
+           {url: '38'
